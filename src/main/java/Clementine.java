47c90316fa2b0d9ca4ac05755ffc41a6fe73c413
zhiyu05 <kohzhiyu5@gmail.com>
2025-08-22 12:04:12 +0800

    completed level 4: todo, events, deadlines

--- src/main/java/Clementine.java
+++ src/main/java/Clementine.java
@@ -16,8 +16,12 @@ public class Clementine {
                 listTasks();
             } else if (input.startsWith("mark ")) {
                 markTask(input);
-            } else if (input.startsWith("unmark ")){
+            } else if (input.startsWith("unmark ")) {
                 unmarkTask(input);
+            } else if (input.startsWith("event ")) {
+                addEventTask(input);
+            } else if (input.startsWith("deadline ")) {
+                addDeadlineTask(input);
             } else {
                 addTask(input);
             }
@@ -42,18 +46,21 @@ public class Clementine {
     }
 
     public static void addTask (String taskDescription) {
-        tasks[taskCount] = new Task(taskDescription);
-        String response = "okay! added: " + taskDescription + " quack!";
+        Task task = new Todo(taskDescription);
+        tasks[taskCount] = task;
+        String response = "okay! ive added the task quack!";
         taskCount++;
         line();
         System.out.println(response);
+        System.out.println(" " + task.toString());
+        System.out.println("now you have " + taskCount + " remaining tasks!");
         line();
     }
 
     public static void listTasks () {
         String response = "tasks to be done before freedom:\n";
         for (int i = 1; i <= taskCount; i++) {
-            response += i + ". " + tasks[i - 1].printTask() + "\n";
+            response += i + ". " + tasks[i - 1].toString() + "\n";
         }
         line();
         System.out.println(response);
@@ -69,7 +76,7 @@ public class Clementine {
                 tasks[taskNumber - 1].taskDone();
                 line();
                 System.out.println("good job! you've completed the task");
-                System.out.println(" " + tasks[taskNumber - 1].printTask());
+                System.out.println(" " + tasks[taskNumber - 1].toString());
                 line();
             } else {
                 System.out.println("invalid task number!");
@@ -88,13 +95,60 @@ public class Clementine {
                 tasks[taskNumber - 1].taskUndone();
                 line();
                 System.out.println("okay, ive changed this task to not done. quack!");
-                System.out.println(" " + tasks[taskNumber - 1].printTask());
+                System.out.println(" " + tasks[taskNumber - 1].toString());
                 line();
             } else {
                 System.out.println("invalid task number");
             }
         } catch (NumberFormatException e) {
             System.out.println("please provide a valid number");
+        }
+    }
+
+    public static void addDeadlineTask(String input) {
+        String[] parts = input.split("/by", 2);
+
+        if (parts.length == 2) {
+            String description = parts[0].substring(9);
+            String deadline = parts[1];
+
+            Task task = new Deadline(description, deadline);
+            tasks[taskCount] = task;
+            String response = "okay! ive added the deadline task quack!";
+            taskCount++;
+            line();
+            System.out.println(response);
+            System.out.println(" " + task.toString());
+            System.out.println("now you have " + taskCount + " remaining tasks!");
+            line();
+        } else {
+            System.out.println("please use the correct format for deadline");
+        }
+    }
+
+    public static void addEventTask (String input) {
+        String[] parts = input.split("/from", 2);
+        if (parts.length == 2) {
+            String description = parts[0].substring(6);
+            String [] timeline = parts[1].split("/to", 2);
+
+            if (timeline.length == 2) {
+                String startTime = timeline[0];
+                String endTime = timeline[1];
+
+                Task task = new Event(description, startTime, endTime);
+                tasks[taskCount] = task;
+                taskCount++;
+                line();
+                System.out.println("okay! ive added the event task for u! quack!");
+                System.out.println(" " + task.toString());
+                System.out.println("now you have " + taskCount + " remaining tasks!");
+                line();
+            } else {
+                System.out.println("please use correct format for event task!");
+            }
+        } else {
+            System.out.println("please use correct format for event task!");
         }
     }
 }
