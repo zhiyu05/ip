4479c37829354f2b1b29ad26d75501d61a1a8aa4
zhiyu05 <kohzhiyu5@gmail.com>
2025-08-26 22:55:51 +0800

    complete level 7: save and load tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,9 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from:" + this.start + " to:" + this.end + ")";
     }
+
+    @Override
+    public String storeData() {
+        return "E | " + super.storeData() + " | /from " + this.start + "/to " + this.end;
+    }
 }
