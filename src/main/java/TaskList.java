65dd9e3aa52a9436508a27029a9bc5939438bb08
zhiyu05 <kohzhiyu5@gmail.com>
2025-08-28 10:52:55 +0800

    extract TaskList class for OOP

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return tasks;
+    }
+
+    public int taskSize() {
+        return tasks.size();
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public void addTask (Task task) throws ClementineException {
+        if (tasks.size() >= 100) {
+            throw new ClementineException("oh quack! the task list is full, please complete some tasks before adding extra!");
+        }
+        tasks.add(task);
+    }
+
+    public void markTask(int index) throws ClementineException{
+        if (tasks.isEmpty()) {
+            throw new ClementineException ("quack! u dont have any tasks yet!");
+        }
+
+        if (index < 1 || index > tasks.size()) {
+            throw new ClementineException("invalid task number!");
+        }
+
+        tasks.get(index - 1).taskDone();
+    }
+
+    public void unmarkTask(int index) throws ClementineException{
+        if (tasks.isEmpty()) {
+            throw new ClementineException ("quack! u dont have any tasks yet!");
+        }
+
+        if (index < 1 || index > tasks.size()) {
+            throw new ClementineException("invalid task number");
+        }
+
+        tasks.get(index - 1).taskUndone();
+    }
+
+    public void deleteTask (int index) throws ClementineException{
+        if (tasks.isEmpty()) {
+            throw new ClementineException("quack! you dont have any tasks to delete!");
+        }
+
+        if (index < 1 || index > tasks.size()) {
+            throw new ClementineException("invalid task number");
+        }
+
+        tasks.remove(index - 1);
+    }
+}
